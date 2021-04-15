62f689d704352b92aeb1d7283714019c798bb3b1
kazuhiro <hatano.kazuhiro817@gmail.com>
2021-04-15 18:32:35 +0900

    Added bootstrap and jobs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
